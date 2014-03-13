fb187dfff2b40993b2cfd21347fc697b567b0c7d
John Greth <jgreth@cmu.edu>
2014-03-13 18:35:22 -0400

    Finished the base code. Prints some defect stuff. Still not in a compiling state.`

--- src/csi/csi.ino
+++ src/csi/csi.ino
@@ -33,56 +33,49 @@
  *
  */
 
+#define DEBUG
+
 // Analog pin defines
-#define RIGHTLEFT 4
-#define TOPBOTTOM 5
-#define NE 0
-#define NW 1
-#define SE 2
-#define SW 3
+#define RIGHTLEFT 0
+#define TOPBOTTOM 1
+#define ACCEL 2
 
 // Digital pin defines
-#define ACCEL_L 4
-#define ACCEL_R 5
-#define L_E1 2
-#define T_E1 3
-#define LR_EN 7
-#define L_SP 6  // pwm
-#define LR_DIR 8
-#define R_SP 9  // pwm
-#define TB_EN 13
+#define RX 0 // Reserved
+#define TX 1 // Reserved
+#define L_E1 2 // Interrupt
+#define T_E1 3 // Interrupt
+#define NE 4
+#define NW 5
+#define SE 6
+#define SW 7
+// NOT_DEFINED 8
+#define LR_SP 9  // pwm
+#define B_SP 10 // pwm
 #define T_SP 11 // pwm
 #define TB_DIR 12
-#define B_SP 10 // pwm
+#define B_EN 13
 
 // Constant defines
+#define HIGH_THRESHOLD 16000
+#define LOW_THRESHOLD 14000
 #define MAX_DEFECT_POINTS 1000
 #define SPEED 192
+#define STALL_TORQUE 128
+#define LR_OFFSET 64
 #define OFFSET 10
 
-#define TOP 0
-#define BOTTOM 1
-#define LEFT 2
-#define RIGHT 3
+#define LEFT LOW
+#define RIGHT HIGH
 
 volatile int state = 0;
 volatile int distance = 0;
 volatile int defect_array_index;
 
-volatile int at_left = 0;
-volatile int at_right = 0;
-volatile int at_top = 0;
-volatile int at_bottom = 0;
-
-volatile int NE_defect = 0;
-volatile int NW_defect = 0;
-volatile int SE_defect = 0;
-volatile int SW_defect = 0;
-
 volatile int adc_pin = 0;
 volatile int adc_val[6] = {0,0,0,0,0,0};
 
-volatile int defect_points[4][MAX_DEFECT_POINTS];
+volatile int defect_points[8][MAX_DEFECT_POINTS];
 
 void setup () {
     // PWM
@@ -93,13 +86,17 @@ void setup () {
     adc_pin = 0;
     ADMUX |= B01000000; ADMUX &= B11010000; ADCSRA |= B11101111;
     
-    // Motors
-    pinMode(L_EN, OUTPUT); pinMode(L_SP, OUTPUT);
-    pinMode(R_EN, OUTPUT); pinMode(R_SP, OUTPUT);
-    pinMode(T_EN, OUTPUT); pinMode(T_SP, OUTPUT);
-    pinMode(B_EN, OUTPUT); pinMode(B_SP, OUTPUT);
-    pinMode(L_E1, OUTPUT); pinMode(T_E1, OUTPUT);
+    // Defect sensing pins
+    pinMode(NE, INPUT); pinMode(NW, INPUT);
+    pinMode(SE, INPUT); pinMode(SW, INPUT);
 
+    // Motors
+    pinMode(LR_SP , OUTPUT);
+    pinMode(B_EN, OUTPUT); pinMode(TB_DIR, OUTPUT);
+    pinMode(T_SP, OUTPUT); pinMode(B_SP, OUTPUT);
+
+    // Encoders
+    pinMode(L_E1, INPUT); pinMode(T_E1, INPUT);
     attachInterrupt(0, enUD, CHANGE);
     attachInterrupt(1, enLR, CHANGE);
 
@@ -110,11 +107,26 @@ void setup () {
 }
 
 void loop() {
-    // TODO: agregate defect data after up/down pass
+    // TODO: Later. Agregate defect data after up/down pass.
+#ifdef DEBUG
+    delay(500);
+    if (digitalRead(NE) == HIGH) printf("NE Defect\n");
+    if (digitalRead(NW) == HIGH) printf("NW Defect\n");
+    if (digitalRead(SE) == HIGH) printf("SE Defect\n");
+    if (digitalRead(SW) == HIGH) printf("SW Defect\n");
+#endif
 }
 
 void enUD() {
-    // TODO: Only needed as a sanity check, will implement later
+    // TODO: Later. Add sanity check.
+
+    // Take note of defects.
+    if ((defect_array_index < MAX_DEFECT_POINTS) && (stage % 3 == 1)) {
+        defect_points[NE][defect_array_index] = digitalRead(NE);
+        defect_points[NW][defect_array_index] = digitalRead(NW);
+        defect_points[SE][defect_array_index] = digitalRead(SE);
+        defect_points[SW][defect_array_index] = digitalRead(SW);
+    }
 }
 
 void enLR() {
@@ -122,20 +134,26 @@ void enLR() {
         distance += 1;
         if (distance > ((3 * WIDTH / 2) + (WIDTH * state))) {
             state += 1;
-            // TODO: make motors go in the right direction
+            digitalWrite(B_EN, LOW);
+            analogWrite(T_SP, 0);
+            analogWrite(B_SP, 0);
+            if (state % 6 == 1) analogWrite(LR_SP, STALL_TORQUE - LR_OFFSET);
+            else                analogWrite(LR_SP, STALL_TORQUE + LR_OFFSET);
         }
     }
 }
 
 ISR(TIMER0_COMPA_vect) {
-    // TODO: periodic tasks go here. Called at 1kHz.
+    // TODO: Later. Add tilt correction.
+    // TODO: Add height correction while moving along top and bottom.
+    // TODO: make corrections use PID
 }
 
 ISR(ADC_vect)
 {
     int analogVal = (ADCL | (ADCH << 8)) << 4;
     int curr_adc_pin = adc_pin;
-    adc_pin = (adc_pin + 1) % 6;
+    adc_pin = (adc_pin + 1) % 3;
 
     // Start the next ADC
     ADMUX &= B11110000; ADMUX |= adc_pin; ADCSRA |= B01000000;
@@ -143,59 +161,23 @@ ISR(ADC_vect)
     adc_val[curr_adc_pin] += (analogVal >> 2) - (adc_val[curr_adc_pin] >> 2);
 
     // Update things that use ADC values
-    switch (curr_adc_pin) {
-        case TOPBOTTOM:
-            switch (state) {
-                case 0: // Moving right at top
-                    // TODO: TB ADC
-                break;
-                case 1: // Moving down
-                    // TODO: TB ADC
-                break;
-                case 2: // Moving left at bottom
-                    // TODO: TB ADC
-                break;
-                case 3: // Moving right at bottom
-                    // TODO: TB ADC
-                break;
-                case 4: // Moving up
-                    // TODO: TB ADC
-                break;
-                case 5: // Moving left at top
-                    // TODO: TB ADC
-                break;
-            }
-            break;
-        case LEFTRIGHT:
-            switch (state) {
-                case 0: // Moving right at top
-                    // TODO: LR ADC
-                break;
-                case 1: // Moving down
-                    // TODO: LR ADC
-                break;
-                case 2: // Moving left at bottom
-                    // TODO: LR ADC
-                break;
-                case 3: // Moving right at bottom
-                    // TODO: LR ADC
-                break;
-                case 4: // Moving up
-                    // TODO: LR ADC
-                break;
-                case 5: // Moving left at top
-                    // TODO: LR ADC
-                break;
-            }
-            break;
-        default:
-            if (state % 3 == 1) {
-                if (defect_array_index < MAX_DEFECT_POINTS) {
-                    defect_points[curr_adc_pin][defect_array_index] = analogVal;
-                }
-                if (curr_adc_pin == 3)
-                    defect_array_index++;
-            }
+    
+    // Moving up/down
+    if ((curr_adc_pin == TOPBOTTOM) && 
+           (((state % 6 == 1) && (adc_val[curr_adc_pin] <  LOW_THRESHOLD)) ||
+            ((state % 6 == 4) && (adc_val[curr_adc_pin] > HIGH_THRESHOLD)))) {
+        state++;
+        analogWrite(LR_SP, STALL_TORQUE);
+        analogWrite(B_SP, SPEED);
+        analogWrite(T_SP, SPEED);
+        digitalWrite(TB_DIR, LEFT);
+        digitalWrite(B_EN, HIGH);
+    }
+    // Moving Left
+    else if (((curr_adc_pin == LEFTRIGHT) && (state % 3 == 2)) &&
+             (adc_val[curr_adc_pin] < LOW_THRESHOLD)) {
+        state++;
+        digitalWrite(TB_DIR, RIGHT);
     }
 }
 
