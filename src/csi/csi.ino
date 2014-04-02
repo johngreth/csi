818fefbec7cbbdfdd36ef02a80f6084cca6c4e8a
John Greth <jgreth@cmu.edu>
2014-04-01 21:19:46 -0400

    .

--- src/csi/csi.ino
+++ src/csi/csi.ino
@@ -47,13 +47,18 @@
 #define T_E1 3 // Interrupt
 #define TB1 5
 #define TB2 6
-#define L2 4
-#define R2 7
-#define LR1 8
+#define L2 9 // 4
+#define R2 10 // 7
+#define LR1 11 // 8
 #define NE 9 
 #define SW 10 
 #define NW 11
 #define SE 12
+
+#define LEFT 0
+#define RIGHT 1
+#define UP 2
+#define DOWN 3
 
 // Constant defines
 #define COLOR_SCALAR 10
@@ -65,7 +70,7 @@
 
 #define MAX_DEFECT_POINTS 1000
 
-#define HIGHSPEED 255
+#define HIGHSPEED 64
 #define LOWSPEED 25
 
 #define STALL_TORQUE 45
@@ -75,12 +80,13 @@
 
 #define DOWNHIGHSPEED2 255
 #define DOWNLOWSPEED2 255
-#define DOWNHIGHSPEED1 255
+#define DOWNHIGHSPEED1 150
 #define DOWNLOWSPEED1 150
 
-#define BUFFER 20
-#define WIDTH 100
-#define HEIGHT 100
+#define BUFFER 100
+#define WIDTH 450
+#define UPHEIGHT 4900
+#define DOWNHEIGHT 3500
 #define SLACK 20
 
 #define PWMRES 15
@@ -93,8 +99,10 @@ volatile int last_state_TB = 0;
 volatile int distance_LR = 0;
 volatile int distance_TB = 0;
 volatile int defect_array_index = 0;
+volatile int next_dist;
 
 volatile int adc_pin = 0;
+volatile unsigned int widths[6];
 volatile unsigned long adc_val[6] = {0,0,0,0,0,0};
 volatile unsigned long raw_val[6] = {0,0,0,0,0,0};
 volatile unsigned long min_val[6] = {0,0,0,0,0,0};
@@ -104,108 +112,28 @@ volatile int defect_points[14][MAX_DEFECT_POINTS];
 
 volatile int pwm_out[14] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-int pwmCtr = 0;
+volatile int pwmCtr = 0;
 
-void badInt() {}
+void en_TB() { distance_TB++; }
 
-// Read defect sensors at regular intervals
-void enLR() {
-    // Moving up and down
-    if ((state % 9 == 3) || (state % 9 == 4) || (state % 9 == 5)) {
-        distance_LR++;
-        if ((state % 9 == 3) && (distance_LR > HEIGHT - BUFFER)) {
-            state++;
-            if (state % 18 < 9) {
-                aWrite(L2, DOWNLOWSPEED2);
-                aWrite(R2, DOWNLOWSPEED2);
-                aWrite(LR1, DOWNLOWSPEED1);
-            }
-            else {
-                aWrite(L2, UPLOWSPEED);
-                aWrite(R2, UPLOWSPEED);
-            }
-        }
-        else if ((state % 9 == 4) && (distance_LR > HEIGHT)) {
-            state++;
-            adc_val[TOPBOTTOM] = (HIGH_THRESHOLD_TB + LOW_THRESHOLD_TB) / 2;
-        }
-        else if ((state % 9 == 5) && (distance_LR > HEIGHT + SLACK)) {
-            state++;
-            aWrite(L2, STALL_TORQUE);
-            aWrite(R2, STALL_TORQUE);
-            aWrite(TB1, HIGHSPEED);
-            distance_TB = 0;
-        }
-        if (defect_array_index < MAX_DEFECT_POINTS) {
-            switch (distance_LR % COLOR_SCALAR) {
-                case 0: //defect_points[NE][defect_array_index] = digitalRead(NE); break;
-                case 1: //defect_points[NW][defect_array_index] = digitalRead(NW); break;
-                case 2: //defect_points[SE][defect_array_index] = digitalRead(SE); break;
-                case 3: //defect_points[SW][defect_array_index] = digitalRead(SW); break;
-                case 4: defect_array_index++; break;
-            }
-        }
-    }
-    
-}
-
-// Move right the correct distance
-void enTB() {
-    // Moving Left or Right
-    if ((state % 9 < 3) || (state % 9 >= 6)) {
-        distance_TB++;
-        if ((state % 9 == 0) && (distance_TB > curr_width - BUFFER)) {
-            state++;
-            aWrite(TB1, LOWSPEED);
-        }
-        else if (((state % 9 == 1) || (state % 9 == 2)) && (distance_TB > curr_width - BUFFER)) {
-            state++;
-            if (state % 9 == 1) state++;
-            aWrite(TB1, 0);
-            if (state % 18 < 9) {
-                aWrite(L2, DOWNHIGHSPEED2);
-                aWrite(R2, DOWNHIGHSPEED2);
-                aWrite(LR1, DOWNHIGHSPEED1);
-            }
-            else {
-                aWrite(L2, UPHIGHSPEED);
-                aWrite(R2, UPHIGHSPEED);
-            }
-            
-        }
-        else if ((state % 9 == 6) && (distance_TB > curr_width - BUFFER - BUFFER)) {
-            state++;
-            aWrite(TB2, LOWSPEED);
-        }
-        else if ((state % 9 == 7) && (distance_TB > curr_width - BUFFER)) { 
-            state++;
-            adc_val[LEFTRIGHT] = (HIGH_THRESHOLD_LR + LOW_THRESHOLD_LR) / 2;
-        }
-        else if ((state % 9 == 8) && (distance_TB > curr_width + SLACK)) {
-            state++;
-            distance_TB = 0;
-            aWrite(TB2, 0);
-            aWrite(TB1, HIGHSPEED);
-        }
-    }
-}
+void en_LR() { distance_LR++; }
 
 void setup () {
     cli();
     Serial.begin(9600);
     // Timer
-    TCNT1  = 0; OCR1A = (PWMFREQ * 1) - 1; TCCR1A = (1 << WGM12);
-    TCCR1B = (1 << CS10); TIMSK1 |= (1 << OCIE1A);
-    TCNT2  = 0; OCR2A = 6                     ; TCCR2A = (1 << WGM21);
-    TCCR2B = (1 << CS22); TIMSK2 |= (1 << OCIE2A);
+    //TCNT1  = 0; OCR1A = (PWMFREQ * 1) - 1; TCCR1A = (1 << WGM12);
+    //TCCR1B = (1 << CS10); TIMSK1 |= (1 << OCIE1A);
+    //TCNT2  = 0; OCR2A = 6                     ; TCCR2A = (1 << WGM21);
+    //TCCR2B = (1 << CS22); TIMSK2 |= (1 << OCIE2A);
     
     // ADC
     adc_pin = 0;
     ADMUX |= B01000000; ADMUX &= B11010000; ADCSRA |= B11101111;
     
     // Defect sensing pins
-    pinMode(NE, INPUT); pinMode(NW, INPUT);
-    pinMode(SE, INPUT); pinMode(SW, INPUT);
+    // pinMode(NE, INPUT); pinMode(NW, INPUT);
+    // pinMode(SE, INPUT); pinMode(SW, INPUT);
 
     // Motors
     pinMode(LR1, OUTPUT); pinMode(R2, OUTPUT); pinMode(L2, OUTPUT);
@@ -213,176 +141,115 @@ void setup () {
     
     pinMode(13, OUTPUT);
     
-    aWrite(LR1, 0); aWrite(L2, 0); aWrite(R2, 0);
-    aWrite(TB2, HIGHSPEED);  aWrite(TB1, 0);
-    distance_LR = 0; distance_TB = 0;
+    pwm_out[L2] = 255;
+    pwm_out[R2] = 255;
+    pwm_out[LR1] = 240;
+    pwm_out[TB1] = 0;
+    pwm_out[TB2] = 80;
+        analogWrite(TB1, pwm_out[TB1]);
+        analogWrite(TB2, pwm_out[TB2]);
+        analogWrite(R2, pwm_out[R2]);
+        analogWrite(L2, pwm_out[L2]);
+        analogWrite(LR1, pwm_out[LR1]);
+    distance_LR = 0;
+    distance_TB = 0;
     
     state = 0;
-    distance_TB = 0;
-    distance_LR = 0;
+    
+    widths[0] = 1*WIDTH/2;
+    widths[1] = 3*WIDTH/2;
+    widths[2] = 5*WIDTH/2;
+    widths[3] = 7*WIDTH/2;
+    widths[4] = 9*WIDTH/2;
+    widths[5] = 11*WIDTH/2;
+    
+    curr_width = widths[(state/18)%3];
+    next_dist = curr_width - BUFFER - BUFFER;
 
     // Encoders
-    //pinMode(L_E1, INPUT); pinMode(T_E1, INPUT);
-    
-    attachInterrupt(0, enTB, RISING);
-    attachInterrupt(1, enLR, RISING);
+    attachInterrupt(1, en_TB, RISING);
+    attachInterrupt(0, en_LR, RISING);
 
     sei();
 }
 
 void loop() {
-
+    int sta = (state % 18);
+    int st = sta / 3;
+    int distance = distance_TB;
+    if (st % 3 == 1) distance = distance_LR;
     
-    // TODO: Later. Agregate defect data after up/down pass.
-
-    delay(250); 
-    digitalWrite(13, HIGH);
-    delay(250);
-    digitalWrite(13, LOW);
-    /*
-    if (digitalRead(NE) == HIGH) Serial.print("NE Defect\n");
-    if (digitalRead(NW) == HIGH) Serial.print("NW Defect\n");
-    if (digitalRead(SE) == HIGH) Serial.print("SE Defect\n");
-    if (digitalRead(SW) == HIGH) Serial.print("SW Defect\n"); */
-    // Serial.print("State: ");
-    // Serial.print(state);
-    //Serial.print(adc_val[0]);
-   //  Serial.print(state);
-    //if (adc_val[0] > 300) Serial.print("!!! DEFECT !!! DEFECT !!! DEFECT !!!! DEFECT !!!\n");
-    //else
-    // Serial.print("\n");
-    
-    Serial.print("\nState: ");
-    Serial.print(state);
-    Serial.print(" TB min: ");
-    Serial.print(min_val[TOPBOTTOM]);
-    Serial.print("  max: ");
-    Serial.print(max_val[TOPBOTTOM]);
-    Serial.print(" LR min: ");
-    Serial.print(min_val[LEFTRIGHT]);
-    Serial.print("  max: ");
-    Serial.print(max_val[LEFTRIGHT]);
-    for (int i = 0; i < 6; i++) {
-        min_val[i] = 0x8000;
-        max_val[i] = 0;
-    }
-}
-
-// PID control
-ISR(TIMER2_COMPA_vect) {
-    // adc_pin = adc_pin % 6;
-    // TODO: Later. Add tilt correction.
-    // TODO: Add height correction while moving along top and bottom.
-    // TODO: make corrections use PID
-}
-
-// PWM timer
-ISR(TIMER1_COMPA_vect) {
-    
-    // Moving up and down
-    if ((state % 9 == 3) || (state % 9 == 4) || (state % 9 == 5)) {
-        distance_LR++;
-        if ((state % 9 == 3) && (distance_LR > HEIGHT - BUFFER)) {
-            state++;
-            if (state % 18 < 9) {
-                aWrite(L2, DOWNLOWSPEED2);
-                aWrite(R2, DOWNLOWSPEED2);
-                aWrite(LR1, DOWNLOWSPEED1);
-            }
-            else {
-                aWrite(L2, UPLOWSPEED);
-                aWrite(R2, UPLOWSPEED);
-            }
+    if (    ((state % 9  ==  8) && (adc_val[LEFTRIGHT] > HIGH_THRESHOLD_LR)) ||
+            ((state % 18 ==  5) && (adc_val[TOPBOTTOM] <  LOW_THRESHOLD_TB)) ||
+            ((state % 18 == 14) && (adc_val[TOPBOTTOM] > HIGH_THRESHOLD_TB)))
+        next_dist = 0;
+        
+    if (distance > next_dist) {
+        state++;
+        sta = (state % 18);
+        st = sta / 3;
+        if (state % 9 == 0) curr_width = widths[(state/9)%6];
+        
+        if (state % 3 == 0) { distance_TB = 0; distance_LR = 0; }
+        
+        pwm_out[L2] = 255;
+        pwm_out[R2] = 255;
+        pwm_out[LR1] = 240;
+        pwm_out[TB1] = 0;
+        pwm_out[TB2] = 0;
+        
+        switch (st) {
+            case 0: case 3: 
+                pwm_out[TB2] = 80;
+            break;
+            case 2: case 5: 
+                pwm_out[TB1] = 60;
+            break;
+            case 1:
+                pwm_out[LR1] = 255;
+                pwm_out[L2] = 240;
+                pwm_out[R2] = 240;
+                break;
+            case 4:
+                pwm_out[LR1] = 100;
+                break;
         }
-        else if ((state % 9 == 4) && (distance_LR > HEIGHT))
-            state++;
-        else if ((state % 9 == 5) && (distance_LR > HEIGHT + SLACK)) {
-            state++;
-            aWrite(L2, STALL_TORQUE);
-            aWrite(R2, STALL_TORQUE);
-            aWrite(LR1, 0);
-            aWrite(TB1, HIGHSPEED);
-            distance_TB = 0;
-        }
-        if (defect_array_index < MAX_DEFECT_POINTS) {
-            switch (distance_LR % COLOR_SCALAR) {
-                case 0: //defect_points[NE][defect_array_index] = digitalRead(NE); break;
-                case 1: //defect_points[NW][defect_array_index] = digitalRead(NW); break;
-                case 2: //defect_points[SE][defect_array_index] = digitalRead(SE); break;
-                case 3: //defect_points[SW][defect_array_index] = digitalRead(SW); break;
-                case 4: defect_array_index++; break;
-            }
-        }
+        
+        analogWrite(TB1, pwm_out[TB1]);
+        analogWrite(TB2, pwm_out[TB2]);
+        analogWrite(R2, pwm_out[R2]);
+        analogWrite(L2, pwm_out[L2]);
+        analogWrite(LR1, pwm_out[LR1]);
+        
+        next_dist = curr_width;
+        if (st == 1) next_dist = DOWNHEIGHT;
+        if (st == 4) next_dist = UPHEIGHT;
+        if (state % 3 == 0) next_dist -= BUFFER + BUFFER;
+        if (state % 3 == 1) next_dist -= BUFFER;
+        if ((state % 9 == 5) && (state % 9 == 8)) next_dist += SLACK;
     }
     
-    
-    pwmCtr = (pwmCtr + PWMRES) % 256;
-    
-    if (pwmCtr < pwm_out[TB1])
-        digitalWrite(TB1, HIGH);
-    else
-        digitalWrite(TB1,  LOW);
-    if (pwmCtr < pwm_out[TB2])
-        digitalWrite(TB2, HIGH);
-    else
-        digitalWrite(TB2,  LOW);
-    if (pwmCtr < pwm_out[L2])
-        digitalWrite(L2, HIGH);
-    else
-        digitalWrite(L2,  LOW);
-    if (pwmCtr < pwm_out[R2])
-        digitalWrite(R2, HIGH);
-    else
-        digitalWrite(R2,  LOW);
-    if (pwmCtr < pwm_out[LR1])
-        digitalWrite(LR1, HIGH);
-    else
-        digitalWrite(LR1,  LOW); 
+    //if (state % 9 == 3) {
+        delayMicroseconds(25000); 
+        digitalWrite(13, HIGH);
+        delayMicroseconds(25000);
+        digitalWrite(13, LOW);
+        
+        Serial.print("\nState: ");
+        Serial.print(state);
+        Serial.print(" st: ");
+        Serial.print(st);
+        Serial.print("  D: ");
+        Serial.print(distance);
+        Serial.print("  ND: ");
+        Serial.print(next_dist);
+    //}
 }
 
 ISR(ADC_vect)
 {
     unsigned long analogVal = (ADCL | (ADCH << 8)) << 4;
-    int curr_adc_pin = adc_pin;
-    /*
-    if (state % 9 == 5) adc_pin = TOPBOTTOM;
-    else if (state % 9 == 8) adc_pin = LEFTRIGHT;
-    else adc_pin = ACCEL;
-    */
+    adc_val[adc_pin] += (analogVal >> 6) - (adc_val[adc_pin] >> 6);
     adc_pin = (adc_pin + 1) % 6;
-    // Start the next ADC
     ADMUX &= B11110000; ADMUX |= adc_pin; ADCSRA |= B01000000;
-    
-    raw_val[curr_adc_pin] = analogVal;
-    adc_val[curr_adc_pin] += (analogVal >> 6) - (adc_val[curr_adc_pin] >> 6);
-    if (adc_val[curr_adc_pin] < min_val[curr_adc_pin]) 
-        min_val[curr_adc_pin] = adc_val[curr_adc_pin];
-    if (adc_val[curr_adc_pin] > max_val[curr_adc_pin])
-        max_val[curr_adc_pin] = adc_val[curr_adc_pin];
-
-    // Update things that use ADC values
-    
-    // Moving up/down
-    if ((curr_adc_pin == TOPBOTTOM) && (
-            ((state % 18 == 5 ) && (adc_val[curr_adc_pin] <  LOW_THRESHOLD_TB)) ||
-            ((state % 18 == 14) && (adc_val[curr_adc_pin] > HIGH_THRESHOLD_TB)))) {
-        state++;
-        aWrite(L2, STALL_TORQUE);
-        aWrite(R2, STALL_TORQUE);
-        aWrite(LR1, 0);
-        aWrite(TB1, HIGHSPEED);
-        distance_TB = 0;
-    }
-    // Moving Left
-    else if (((curr_adc_pin == LEFTRIGHT) && (state % 9 == 8)) &&
-             (adc_val[curr_adc_pin] > HIGH_THRESHOLD_LR)) {
-        state++;
-        distance_TB = 0;
-        aWrite(TB2, 0);
-        aWrite(TB1, HIGHSPEED);
-    }
-}
-
-void aWrite(int pin, int sp) {
-    pwm_out[pin] = sp;
 }
